84c21a00cd18739a8ebfd08b5a7df29e921f23cf
Téo Muller-Heddar <teomullerheddar@gmail.com>
2025-01-15 14:28:45 +0100

    init project

---
+++ src/main/java/fr/tetemh/jumpPlug/JumpPlug.java
@@ -0,0 +1,16 @@
+package fr.tetemh.jumpPlug;
+
+import de.oliver.fancyholograms.api.FancyHologramsPlugin;
+import de.oliver.fancyholograms.api.HologramManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JumpPlug extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
